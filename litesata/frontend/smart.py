aed799e63b3dc964296b394d0d933439aafbd906
Ryohei Niwase <ryohei@niwase.net>
2024-05-13 14:34:08 +0900

    frontend/smart.py: add LiteSATASMARTReadDataDMA

--- litesata/frontend/smart.py
+++ litesata/frontend/smart.py
@@ -4,9 +4,13 @@
 # Copyright (c) 2023 Ryohei Niwase <niwase@lila.cs.tsukuba.ac.jp>
 # SPDX-License-Identifier: BSD-2-Clause
 
+from math import log2
+
 from litesata.common import *
 
+from litex.gen import *
 from litex.soc.interconnect.csr import *
+from litex.soc.cores.dma import WishboneDMAWriter
 
 # LiteSATASMARTReadData ----------------------------------------------------------------------------
 
@@ -78,3 +82,84 @@ class LiteSATASMARTReadData(Module, AutoCSR):
             self._source_data.status.eq(self.source.data),
             self.source.ready.eq(self._source_ready.r & self._source_ready.re)
         ]
+
+# LiteSATASMARTReadDataDMA -------------------------------------------------------------------------
+
+class LiteSATASMARTReadDataDMA(Module, AutoCSR):
+    def __init__(self, port, bus, endianness="little"):
+        self.port     = port
+        self.bus      = bus
+        self.base     = CSRStorage(64)
+        self.start    = CSR()
+        self.done     = CSRStatus()
+        self.error    = CSRStatus()
+        self.irq      = Signal()
+
+        # # #
+
+        port_bytes = port.dw//8
+        dma_bytes  = bus.data_width//8
+        count      = Signal(max=logical_sector_size//dma_bytes)
+        crt_base   = Signal(64)
+
+        # Sector buffer
+        buf = stream.SyncFIFO([("data", port.dw)], logical_sector_size//port_bytes)
+        self.submodules.buf = buf
+
+        # Converter
+        conv = stream.Converter(nbits_from=port.dw, nbits_to=bus.data_width)
+        self.submodules.conv = conv
+
+        # Connect Port to Sector Buffer
+        self.comb += port.source.connect(buf.sink, keep={"valid", "ready", "last", "data"})
+
+        # Connect Sector Buffer to Converter
+        self.comb += buf.source.connect(conv.sink)
+
+        # DMA
+        dma = WishboneDMAWriter(bus, with_csr=False, endianness=endianness)
+        self.submodules.dma = dma
+
+        # Control FSM
+        self.submodules.fsm = fsm = FSM(reset_state="IDLE")
+        fsm.act("IDLE",
+            If(self.start.re,
+                NextValue(count,             0),
+                NextValue(crt_base,          self.base.storage),
+                NextValue(self.error.status, 0),
+                NextState("SEND-CMD")
+            ).Else(
+                self.done.status.eq(1)
+            ),
+            conv.source.ready.eq(1)
+        )
+        fsm.act("SEND-CMD",
+            port.sink.valid.eq(1),
+            port.sink.last.eq(1),
+            port.sink.smart_read_data.eq(1),
+            If(port.sink.ready,
+                NextState("RECEIVE-DATA-DMA")
+            )
+        )
+        fsm.act("RECEIVE-DATA-DMA",
+            # Connect Converter to DMA.
+            dma.sink.valid.eq(conv.source.valid),
+            dma.sink.last.eq(conv.source.last),
+            dma.sink.address.eq(crt_base[int(log2(dma_bytes)):] + count),
+            dma.sink.data.eq(reverse_bytes(conv.source.data)),
+            conv.source.ready.eq(dma.sink.ready),
+            If(dma.sink.valid & dma.sink.ready,
+                NextValue(count, count + 1),
+                If(dma.sink.last,
+                    self.irq.eq(1),
+                    NextState("IDLE")
+                )
+            ),
+
+            # Monitor errors
+            If(port.source.valid & port.source.ready & port.source.failed,
+                self.irq.eq(1),
+                NextValue(self.error.status, 1),
+                NextState("IDLE"),
+            )
+        )
